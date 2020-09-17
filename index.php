a48d10bc2f2e67144fac02fd5e716d1500fb91ee
aryalrajan18 <71181375+aryalrajan18@users.noreply.github.com>
2020-09-17 14:53:15 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,121 @@
+@import url('https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@1,700&display=swap');
+.fplayer{
+    display: block;
+    height: 40px;
+    width: justify;
+    text-align: center;
+    background-color:lightslategrey;
+    font-family: 'Source Serif Pro', serif;
+    font-size: 2em;
+    text-shadow: 3px 3px 5px #DF6589FF ;
+    color: white;
+    margin: 0px;
+    padding: 0px;
+}
+*{
+    margin: 0px;
+    padding: 0px;
+}
+.main{
+  width: 90vh;
+}
+*:before,
+*:after {
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Nunito', sans-serif;
+  color: #384047;
+}
+
+form {
+  position: relative;
+  padding: 1px 12px;
+  background: #12343b;
+  border-radius: 8px;
+  margin: 2px 10px;
+  border: 1px none none;
+  left: 50%;
+}
+
+input[type="text"],
+input[type="phone"],
+input[type="email"],
+input[type="number"],
+select {
+  background: rgba(255, 255, 255, 0.1);
+  border: none;
+  font-size: 16px;
+  height: 100%;
+  margin: 0;
+  outline: 0;
+  padding: 1px;
+  widows: 50px;
+  background-color:#97bc62ff;
+  color: black;
+  box-shadow: 0 1px 0 rgba(0, 0, 0, 0.03) inset;
+  margin-bottom: 30px;
+  width: 100%;
+}
+
+input[type="radio"]
+{
+  margin: 0 4px 8px 0;
+}
+
+label {
+  display: block;
+  width: 100%; 
+  color: aqua;
+}
+
+label.light {
+  font-weight: 300;
+  display: inline;
+  padding: 2px;
+}
+button{
+  font: 'Source Serif Pro', serif;
+  text-transform: uppercase;
+  overflow: hidden;
+  position: relative;
+  padding: 11px 49px;
+  left: 50%;
+  margin: 17px;
+}
+a{
+  color: rosybrown;
+  text-decoration: none;
+}
+.insert{
+  background-color:lawngreen;
+}
+.refresh{
+  background-color: grey;
+
+}
+.info{
+  text-align: center;
+  text-shadow: 2px 2px 2px #F7C331;
+
+  ;
+  
+}
+.putshow{
+  display: flex;
+}
+
+.wrapper{
+  display: block;
+  height: 100%;
+  width: 133%;
+  margin: 5px;
+  padding: 5px;
+}
+.high{
+  background-color: lightblue;
+  height: 90vh;
+}
